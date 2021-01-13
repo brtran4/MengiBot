3e0f9cf940301db0ba2baa237c2943e10c93682b
brtran <brtran@umich.edu>
2021-01-13 09:54:36 +0000

    added a weather command from a weather api

--- main.py
+++ main.py
@@ -8,6 +8,41 @@ from keep_alive import keep_alive
 
 client = discord.Client()
 
+
+baseURL = "http://api.openweathermap.org/data/2.5/weather?"
+
+APIKEY = "8b7f86aa407415a8493c1c4f8926ac64"
+
+def weather(city):
+  cityName = city
+  completeURL = baseURL +  "q=" + cityName + "&appid=" + APIKEY
+  print(completeURL)
+  response = requests.get(completeURL)
+  x = response.json()
+
+  if x["cod"] != "404": # error checking if city is valid
+      y = x["main"]
+      currentTemp = y["temp"]
+      currentTempCelsius = str(round(currentTemp - 273.15))
+      currentPressure = y["pressure"]
+      currentHumidity = y["humidity"]
+      z = x["weather"]
+      weatherDescription = z[0]["description"]
+
+      title = "Current weather in " + cityName + ":"
+      description = "\nDescripition: " + str(weatherDescription)
+      ftemp= "\nTemperature(F): " + str(int(currentTempCelsius) * (9/5) + 32) + "°F"
+      ctemp = "\nTemperature(C): " + str(currentTempCelsius) + "°C"
+      humidity ="\nHumidity(%): " + str(currentHumidity) + "%"
+      pressure ="\nAtmospheric Pressure(hPa): " + str(currentPressure) + "hPa"
+
+      return title + description + ftemp + ctemp + humidity + pressure
+  else:
+      return "City not found."
+
+
+
+
 sadWords = ["sad", "depressed", "unhappy"]
 
 starterEncouragements = [
@@ -24,6 +59,8 @@ def getQuote():
   jsonData = json.loads(response.text)
   quote = jsonData[0]['q'] + " -" + jsonData[0]['a']
   return quote
+
+
 
 def updateEncouragements(encouragingMessage):
   if "encouragements" in db.keys():
@@ -90,6 +127,11 @@ async def on_message(message):
     else:
       db["responding"] = False
       await message.channel.send("Responding is off.")
+
+  if msg.startswith("!weather"):
+    cityName = msg.split("!weather ", 1)[1]
+    print(cityName)
+    await message.channel.send(weather(cityName))
 
 keep_alive()
 client.run(os.getenv("TOKEN"))
